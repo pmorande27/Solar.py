a054dc2e4f742bceeab290a659aef9c4b27d4b57
pmorande27 <pmorande27@gmail.com>
2020-09-30 17:41:36 +0100

    Create Vector.py file including a class to describe vectors and its operations

---
+++ vector.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 17:33:33 2020
+
+@author: Pablo Morandé
+"""
+import math
+class Vector(object):
+    def __init__(self,value_x,value_y):
+        self.value_x = value_x
+        self.value_y = value_y
+    def get_x(self):
+        return self.value_x
+    def get_y(self):
+        return self.value_y
+    def mdoulus(self): 
+        return math.sqrt(self.value_x**2+ self.value_y**2)
+    @staticmethod
+       
+    def distance(vector1, vector2):
+        result = Vector (vector2.get_x - vector1.get_x, vector2.get_y - vector1.get_y)
+        return result
+
+        
