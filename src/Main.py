a7df0f8da04ba1ee03030db11e3b30d82f4fcb62
Pablo Morandé <pmorande27@gmail.com>
2021-04-03 21:37:32 +0200

    delete vector.py

--- src/Main.py
+++ src/Main.py
@@ -3,7 +3,6 @@ from Planet import Planet
 from SolarSystem import SolarSystem
 import numpy as np
 import matplotlib
-
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from helperfunctions import writeFile
