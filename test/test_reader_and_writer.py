ba8ee3a47c172444f18cefea0ff658a9cdac63fb
Pablo Morandé <pmorande27@gmail.com>
2021-04-05 14:24:52 +0200

    Complete test on add planet

--- test/test_reader_and_writer.py
+++ test/test_reader_and_writer.py
@@ -9,22 +9,29 @@ from Options import Options
 
 
 class TestReader(unittest.TestCase):
+
+    def setUp(self):
+        helperfunctions.writeFile("test")
+
+
+    def tearDown(self):
+        os.remove("../data/test")
+
     
     def test_WrittingFile(self):
-        helperfunctions.writeFile("test")
         system = SolarSystem(1,1,Options.PROBE_RUN,"test")
         self.assertEqual(len(system.celestial_bodies),6)
-        os.remove("../data/test")
     
     def test_Adding_Planet(self):
-        helperfunctions.writeFile("test")
         helperfunctions.add_planet("test",1,1,1,"test")
         system = SolarSystem(1,1,Options.PROBE_RUN,"test")
         self.assertEqual(len(system.celestial_bodies),7)
         self.assertEqual(system.celestial_bodies[len(system.celestial_bodies)-1].name,"test")
         self.assertEqual(system.celestial_bodies[len(system.celestial_bodies)-1].mass,1)
-        helperfunctions.writeFile("test")
-        os.remove("../data/test")
+        self.assertEqual(system.celestial_bodies[len(system.celestial_bodies)-1].orbital_radius,1)
+        self.assertEqual(system.celestial_bodies[len(system.celestial_bodies)-1].type_of_object,"Planet")
+    
+
 
 if __name__ == "__main__":
     unittest.main()
