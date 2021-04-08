a9656f879bdc5e9c2a70860723a36faedabfd29d
pmorande27 <pmorande27@gmail.com>
2021-04-08 16:35:59 +0200

    Add colour

--- src/utils/helperfunctions.py
+++ src/utils/helperfunctions.py
@@ -7,12 +7,12 @@ def writeFile(filename="CelestialObjects"):
     """Method used to write in the files the usual planets used in the project
     """
     path = './data/'+filename
-    sun = Planet("Sun",1.989 * 10**30,0,3*500000000,"Star",0,0)
-    mercury = Planet("Mercury",3.285 * 10**23,58*10**9,2*500000000,"Planet",1.989 * 10**30,0)
-    Venus = Planet("Venus",4.867 *10**24 ,108200000*10**3,2*500000000,"Planet",1.989 * 10**30,0)
-    earth = Planet("Earth",5.972 *10**24,149597870.691*10**3,2*500000000,"Planet",1.989 * 10**30,0)
-    mars = Planet("Mars",6.39 *10**23,228 *10**9,2*500000000,"Planet",1.989 * 10**30,0)
-    Probe = Planet("Probe",1,149597870.691*10**3,2*500000000,"Probe",1.989 * 10**30,0)
+    sun = Planet("Sun",1.989 * 10**30,0,3*500000000,"Star",0,0,"yellow")
+    mercury = Planet("Mercury",3.285 * 10**23,58*10**9,2*500000000,"Planet",1.989 * 10**30,0,"orange")
+    Venus = Planet("Venus",4.867 *10**24 ,108200000*10**3,2*500000000,"Planet",1.989 * 10**30,0,"brown")
+    earth = Planet("Earth",5.972 *10**24,149597870.691*10**3,2*500000000,"Planet",1.989 * 10**30,0,"blue")
+    mars = Planet("Mars",6.39 *10**23,228 *10**9,2*500000000,"Planet",1.989 * 10**30,0,"red")
+    Probe = Planet("Probe",1,149597870.691*10**3,2*500000000,"Probe",1.989 * 10**30,0,"black")
     d =[sun,mercury,Venus,earth,mars,Probe]
     data = {}
     data['Star'] = []
@@ -21,6 +21,7 @@ def writeFile(filename="CelestialObjects"):
             "orbital_radius" : "0",
             "simulated_radius" : str(sun.simulated_radius),
             "type": sun.type_of_object,
+            "colour":sun.colour
             })
     data['Planets'] = []
     for i in range(1,len(d)):
@@ -30,10 +31,11 @@ def writeFile(filename="CelestialObjects"):
             "orbital_radius" : str(planet.orbital_radius),
             "simulated_radius" : str(planet.simulated_radius),
             "type": planet.type_of_object,
+            "colour":planet.colour
             })
     with open(path, "w") as outfile:
         json.dump(data, outfile)
-def add_planet(name,mass,orbital_radius,sim_radius,filename = "CelestialObjects"):
+def add_planet(name,mass,orbital_radius,sim_radius,colour,filename = "CelestialObjects"):
     """Method used to add a new Celestial Body to the file of CelestialBodies.txt in the correct json format.
     """
     planet_name = name
@@ -49,7 +51,8 @@ def add_planet(name,mass,orbital_radius,sim_radius,filename = "CelestialObjects"
             "mass" :str(planet_mass),
             "orbital_radius" : str(planet_orbital_radius),
             "simulated_radius" : str(simulated_radius),
-            "type": "Planet"
+            "type": "Planet",
+            "colour":colour
             })
     with open(path, "w") as outfile:
         json.dump(data, outfile)
